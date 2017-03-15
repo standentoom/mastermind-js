0c3726fc13ba3e0ada3cd984970729aff36b5e41
DESKTOP-EM2NVUS\tooms <99041312@mydavinci.nl>
2017-03-15 10:54:18 +0100

    no message

---
+++ js/mastermind.js
@@ -0,0 +1,24 @@
+// geheime kleuren
+var geheimeKleur1 =1 +Math.floor(( Math.random() * 6));
+var geheimeKleur2 =1 +Math.floor(( Math.random() * 6));
+var geheimeKleur3 =1 +Math.floor(( Math.random() * 6));
+var geheimeKleur4 =1 +Math.floor(( Math.random() * 6));
+
+var poging = 0;
+
+console.log(geheimeKleur1)
+console.log(geheimeKleur2)
+console.log(geheimeKleur3)
+console.log(geheimeKleur4)
+
+function raad() {
+//haal de gekozen kleuren op
+var raad1 = document.getElementById
+
+// kopieer de gekozen kleuren naar een nog ingebruikte rij
+
+// controleer welk van de gekozen kleuren op de goede plek
+}
+
+
+
